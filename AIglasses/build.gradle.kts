8bc6886416ba49c7f3595400f125a039091b4660
Junsheng Huang <84214879+Dancing-Github@users.noreply.github.com>
2024-11-13 13:38:50 +0800

    Add files via upload

---
+++ AIglasses/build.gradle.kts
@@ -0,0 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    kotlin("jvm") version "1.9.21"
+}
+
+dependencies {
+//    implementation(kotlin("stdlib-jdk8"))
+}
+repositories {
+//    mavenCentral()
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "17"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "17"
+}
